8e6f7b4cf22b11712bf65e04b1f5931a52357818
Yanisa Thaenthong <6410110649@psu.ac.th>
2022-03-25 15:35:56 +0700

    up

--- nsc-be/nsc/core/urls.py
+++ nsc-be/nsc/core/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from django.urls.conf import include
 # from rest_framework import routers
 from rest_framework.routers import DefaultRouter
-from .views import Adminupload1Viewset, Adminupload2Viewset, CertificateViewset, whoami, NewUserViewset
+from .views import Adminupload1Viewset, Adminupload2Viewset, CertificateViewset, UserdetailViewset, whoami, NewUserViewset
 
 # routers = routers.DefaultRouter()
 router = DefaultRouter()
 router.register(r"adminupload1", Adminupload1Viewset, basename='adminview1')
 router.register(r"adminupload2", Adminupload2Viewset, basename='adminview2')
 router.register(r"newuser", NewUserViewset, basename='newuser')
-# router.register(r"userdetail", UserdetailViewset, basename='userdetail')
+router.register(r"userdetail", UserdetailViewset, basename='userdetail')
 router.register(r"certificate", CertificateViewset, basename='certificate')
 
 
