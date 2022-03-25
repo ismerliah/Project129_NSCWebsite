7510f47361e69a7ae65a88d50afc69ac3ed84c6f
Yanisa Thaenthong <6410110649@psu.ac.th>
2022-03-25 16:07:44 +0700

    .

--- nsc-be/nsc/core/views.py
+++ nsc-be/nsc/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Adminupload1, Adminupload2, Certificate, NewUser
-from .serializers import CertificateSerializer, NewUserSerializer, Adminupload1Serializer, Adminupload2Serializer, ProjectdetailSerializer
+from .serializers import CertificateSerializer, NewUserSerializer, Adminupload1Serializer, Adminupload2Serializer
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
